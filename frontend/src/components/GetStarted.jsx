2dc5d6d8984afe9f2b06a03eacf38897376f1848
Ayush Deb <ayushdeb848@gmail.com>
2025-01-16 18:20:02 +0530

    fixed for smaller screens

--- frontend/src/components/GetStarted.jsx
+++ frontend/src/components/GetStarted.jsx
@@ -86,7 +86,7 @@ const GetStarted = () => {
           {!showActions && (
             <button
               onClick={handleSubmit}
-              className={`px-4 py-2 rounded-lg bg-blue-600 hover:bg-blue-700 text-white font-medium transition disabled:opacity-50 ${
+              className={`px-4 py-2 sm:px-6 sm:py-3 rounded-lg bg-blue-600 hover:bg-blue-700 text-white font-medium transition disabled:opacity-50 ${
                 loading ? "cursor-not-allowed" : ""
               }`}
               disabled={loading}
