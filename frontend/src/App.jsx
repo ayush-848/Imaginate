5611bce3a1292fd083c7974972ddf5c6069e211a
Ayush Deb <ayushdeb848@gmail.com>
2025-01-19 01:48:36 +0530

    update

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,6 +5,7 @@ import webfont from 'webfontloader';
 import SignUp from "./pages/SignUp";
 import Login from "./pages/Login";
 import AuthProvider from "./context/authContext";
+import { ToastContainer } from "react-toastify";
 
 
 
@@ -20,6 +21,7 @@ const App = () => {
   return (
     <Router>
         <AuthProvider>
+          <ToastContainer/>
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/signup" element={<SignUp/>} />
